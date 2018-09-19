83a97948dc8f3acb15cce83c85ae980f4a8b77a4
tomganchos <tomganchos@mail.ru>
2018-09-19 23:33:37 +0300

    update grid: add mobile test version

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
     <div style="display: flex; max-width: 1200px; padding: 0 5px; margin: auto">
-      <div class="hidden-xs" style="margin: 5px 0">
-        <left-menu></left-menu>
+      <div>
+        <div class="hidden-xs" style="margin: 5px 0">
+          <left-menu></left-menu>
+        </div>
+        <div class="visible-sm" style="margin: 5px 0">
+          <right-menu></right-menu>
+        </div>
       </div>
-      <div style="background-color: #0E566C; width: 100%; margin: 5px">
+
+      <div class="ui menu visible-xs" style="width: 100%; position: fixed; margin-left: -5px; margin-top: 1px; border-radius: 0 0 .28571429rem .28571429rem;">
+        <a class="header item" style="display: inline-block">
+          Бургер
+        </a>
+        <a class="item" style="display: inline-block">
+          Лого
+        </a>
+      </div>
+
+      <div class="center-menu">
         <router-view/>
       </div>
       <div class="visible-md visible-lg" style="margin: 5px 0">
-        <left-menu></left-menu>
+        <right-menu></right-menu>
       </div>
     </div>
 
@@ -17,9 +32,10 @@
 
 <script>
 import LeftMenu from "@/components/menu/LeftMenu";
+import RightMenu from "@/components/menu/RightMenu";
 export default {
   name: 'App',
-  components: {LeftMenu}
+  components: {RightMenu, LeftMenu}
 }
 </script>
 
@@ -29,6 +45,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+.center-menu {
+  background-color: #0E566C;
+  width: 100%;
+  margin: 5px;
+}
+@media screen and (max-width: 768px) {
+  .center-menu {
+    margin-top: 40px;
+  }
 }
   /*.hidden-xs, .visible-lg {*/
     /*display: inline-block !important;*/
