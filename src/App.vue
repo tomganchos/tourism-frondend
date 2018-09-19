c84ba44f6838ff40d44cc0d115b013018e23e0c5
tomganchos <tomganchos@mail.ru>
2018-09-19 08:46:07 +0300

    add grid for adaptive ui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <div style="display: flex; max-width: 1200px">
-      <div class="hidden-xs" style="margin: 5px">
+    <div style="display: flex; max-width: 1200px; padding: 0 5px; margin: auto">
+      <div class="hidden-xs" style="margin: 5px 0">
         <left-menu></left-menu>
       </div>
-      <div style="background-color: #0E566C;">
+      <div style="background-color: #0E566C; width: 100%; margin: 5px">
         <router-view/>
       </div>
-      <div class="visible-md visible-lg" style="margin: 5px">
+      <div class="visible-md visible-lg" style="margin: 5px 0">
         <left-menu></left-menu>
       </div>
     </div>
