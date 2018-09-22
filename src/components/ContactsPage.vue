c440259bed559e0ca80613eb22f0f0df9aa25daf
tomganchos <tomganchos@mail.ru>
2018-09-22 12:02:59 +0300

    add ContactsPage, MethLibraryPage and Page404

---
+++ src/components/ContactsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="ui segment head">
+      <div class="ui large header">Контакты</div>
+    </div>
+    <div class="ui segment body">
+      <div class="text">Адрес:  <b>г.Псков, ул.Народная, 53</b></div>
+      <div class="text">Индекс:  <b>180016</b></div>
+      <div class="text">Приёмная директора:  <b>+7 (8112) 57-60-84</b></div>
+      <div class="text">Заместитель директора:  <b>+7 (8112) 57-62-53</b></div>
+      <div class="text">Бухгалтерия:  <b>+7 (8112) 57-62-53</b></div>
+      <div class="text">Общежитие:  <b>+7 (8112) 57-62-54</b></div>
+      <div class="text">Электронная почта: <b>org608@pskovedu.ru</b></div>
+      <div class="ui medium header text">Расположение на карте:</div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "PageContacts"
+    }
+</script>
+
+<style scoped>
+  .ui.segment.head {
+    margin-bottom: 5px;
+  }
+  .ui.segment .header {
+    color: #204d74;
+  }
+  .ui.segment.body {
+    margin-top: 0;
+  }
+</style>
---
+++ src/components/MethLibraryPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <div class="ui segment head">
+        <div class="ui large header">Методическая библиотека</div>
+      </div>
+      <div class="ui segment body">
+        <div class="ui relaxed divided list">
+          <div class="item">
+            <div class="item-icon">
+              <i class="file alternate outline icon"></i>
+            </div>
+            <div class="content">
+              <a class="header" href="https://drive.google.com/file/d/0B4Y-eduzf4rjaDdWWmRqMUVEZWM/view?usp=sharing" target="_blank" rel="noopener">Методические рекомендации по организации научно-исследовательской деятельности обучающихся</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MethLibraryPage"
+    }
+</script>
+
+<style scoped>
+  .ui.segment.head {
+    margin-bottom: 5px;
+  }
+  .ui.segment .header {
+    color: #204d74;
+  }
+  .ui.segment.body {
+    margin-top: 0;
+  }
+  .ui.segment.body .item a {
+    line-height: 1.3;
+  }
+  .ui.segment.body .item {
+    display: flex;
+  }
+  .item-icon {
+    display: flex;
+    align-items: center;
+  }
+  .item-icon i {
+    margin-right: 10px;
+  }
+  .item .content {
+    display: flex;
+  }
+</style>
