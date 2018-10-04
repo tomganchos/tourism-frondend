87cfc0bfbfc04034a5e0846ef1d9c9f057c7b66c
tomganchos <tomganchos@mail.ru>
2018-10-04 23:52:55 +0300

    update LeftMenu (Documents of Center)

--- src/components/menu/LeftMenu.vue
+++ src/components/menu/LeftMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ui left vertical menu">
+    <nav class="ui left vertical menu">
       <router-link to="/" class="item" style="text-align: -webkit-center">
         <img src="http://turizmpskov.ru/image/logoTurizm/logoTurizm2.png" height="150px">
       </router-link>
@@ -12,36 +12,64 @@
           <span>Сведения об образовательной организации</span>
         </a>
         <div class="content">
-          <p class="transition visible" style="display: block !important;">A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-        </div>
-        <a class="title item">
-          <i class="dropdown icon"></i>
-          <span>Сведения об образовательной организации</span>
-        </a>
-        <div class="content">
-          <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+          <p>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Документы Центра</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Структура</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Материально-техническое обеспечение и оснащённость образовательного процесса</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Финансово-хозяйственная деятельность</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Отчёт о результатах самообследования</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Платные образовательные услуги</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Нормативная база</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Основные сведения</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Предприсания органов, осуществляющих Государственный контроль (надзор) в сфере образования</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Образовательная деятельность</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Образовательные стандарты</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Стипендии и иные виды материальной поддержки</span>
+            </router-link>
+            <router-link to="/documents-of-center" class="sub-item">
+              <span>Вакантные места для приёма (перевода)</span>
+            </router-link>
+          </p>
         </div>
         <a class="title item">
           <i class="dropdown icon"></i>
           <span>Руководство. Педагогический состав. Сотрудники</span>
         </a>
         <div class="content">
+          <p>There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+        </div>
+        <a class="title item">
+          <i class="dropdown icon"></i>
+          <span>Отделы Центра</span>
+        </a>
+        <div class="content">
           <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
           <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
         </div>
       </div>
-      <a class="item">
-        <i class="angle down"></i>
-        <span>Сведения об образовательной организации</span>
-      </a>
-      <a class="item">
-        <i class="angle down"></i>
-        <span>Руководство. Педагогический состав. Сотрудники</span>
-      </a>
-      <a class="item">
-        <i class="angle down"></i>
-        <span>Отделы Центра</span>
-      </a>
       <router-link to="touristroutes" class="item">
         <span>Туристские маршруты</span>
       </router-link>
@@ -78,8 +106,7 @@
       <router-link to="contacts" class="item">
         <span>Контакты</span>
       </router-link>
-
-    </div>
+    </nav>
 </template>
 
 <script>
@@ -108,19 +135,35 @@
     text-align: center;
     line-height: 1.3;
   }
-  .vertical.menu a.item:hover {
+  .vertical.menu a.item:hover, .vertical.menu a.item:focus {
     background-color: #286090;
     border-color: #204d74;
-    color: #fff;
-  }
-  .vertical.menu a.item:focus {
-    background-color: #204d74;
-    border-color: #122b40;
     color: #fff;
   }
   .vertical.menu a.item:active {
     background-color: #204d74;
     border-color: #122b40;
     color: #fff;
+  }
+
+  .vertical.menu a.sub-item {
+    display: flex;
+    padding: 3px 10px;
+    justify-content: center;
+  }
+  .vertical.menu a.sub-item:hover, .vertical.menu a.sub-item:focus {
+    background-color: #edeef0;
+    text-decoration: none;
+  }
+  .vertical.menu a.sub-item:active {
+    background-color: #d2d4d9;
+    text-decoration: none;
+  }
+  .vertical.menu a.sub-item span {
+    text-align: center;
+  }
+  .vertical.menu div.content {
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
   }
 </style>
