37235cbdadbf5802c7545a7633fdb8d582c4f5e0
tomganchos <tomganchos@mail.ru>
2018-10-08 13:50:11 +0300

    add MaterialTechProvision component

---
+++ src/components/MaterialTechProvision.vue
@@ -0,0 +1,261 @@
+<template>
+  <div>
+    <div class="ui segment head">
+      <div class="ui medium header">Материально-техническое обеспечение</div>
+    </div>
+    <div class="ui segment body">
+      <div class="ui small header" style="color: #204d74">Кабинеты</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Кабинет директора
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Кабинет заместителя директора
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Приемная
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Методический кабинет
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Кабинет музейной и экскурсионной работы и хозяйственный отдел
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Отдел по туризму
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Бухгалтерия
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui segment body">
+      <div class="ui small header" style="color: #204d74">Помещения</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Спортивный зал
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Учебный класс
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Склад снаряжения
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Архив
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Общежитие
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Музей истории народного образования Псковской области (ул.Народная, 20, здание технологического техникума)
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui segment body">
+      <div class="ui small header" style="color: #204d74">Наглядные пособия</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Узлы, навесная переправа, этапы ТПТ, виды спортивных карт, компасы
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Исследовательские, творческие работы по краеведению, экскурсионные и туристские маршруты
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Спортивные карты
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Журналы «Внешкольник», «Дополнительное образование», «Вестник туризма», методическая литература, образовательные программы
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Методические разработки по патриотическому направлению
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Краеведческая литература
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui segment body">
+      <div class="ui small header" style="color: #204d74">Техника</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Спортивные тренажеры
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Теннисный стол
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Компьютерная техника и оргтехника
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Теле–видео аппаратура
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Аудио аппаратура
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Мультимедийные установки, экраны
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Ноутбуки
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Наличие интернета
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Наличие транспорта-автомашины УАЗ3909 и ИЖ 2717
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui segment body">
+      <div class="ui small header" style="color: #204d74">Музей истории народного образования Псковской области</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Кабинет руководителя музея
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Экспозиции с витринами и стендами по истории образования
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Уголок школьника
+          </div>
+        </div>
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Методические материалы смотров-конкурсов школьных музеев и недели «Музей и дети» за несколько лет
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui segment body" style="margin-bottom: 15px">
+      <div class="ui small header" style="color: #204d74">Склад снаряжения</div>
+      <div class="ui list">
+        <div class="item">
+          <i class="bullseye icon"></i>
+          <div class="content">
+            Снаряжение (палатки, спальные мешки, коврики, надувные матрасы,
+            складная туристская мебель, компасы, страховочные системы, шнуры,
+            карабины, секундомеры, котелки, лодки моторные, генераторы, крематы, спорт. инвентарь и т.п.)
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "MaterialTechProvision"
+    }
+</script>
+
+<style scoped>
+  .ui.segment.head {
+    margin-bottom: 5px;
+  }
+  .ui.segment .header {
+    color: #204d74;
+  }
+  .ui.segment.body {
+    margin-top: 0;
+    margin-bottom: 5px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import News from '@/components/News'
+import MaterialTechProvision from '@/components/MaterialTechProvision'
 import PublicationPage from '@/components/PublicationPage'
 import MethLibraryPage from '@/components/MethLibraryPage'
 import AntiCorruption from '@/components/AntiCorruption'
@@ -17,6 +18,11 @@ export default new Router({
       path: '/',
       name: 'News',
       component: News
+    },
+    {
+      path: '/material-tech-provision',
+      name: 'MaterialTechProvision',
+      component: MaterialTechProvision
     },
     {
       path: '/publications',
