95efff6e85402d7a000d14d221fddd1580646376
tomganchos <tomganchos@mail.ru>
2018-10-13 14:45:31 +0300

    Add Scolarshops

---
+++ src/components/ScholarshipsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="ui segment head">
+      <h1 class="ui medium header">Стипендии и иные виды материальной поддержки</h1>
+    </div>
+    <div class="ui segment body">
+      <div class="ui small header">Стипендий и материальной поддержки обучающимся не предоставляется</div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ScholarshipsPage"
+    }
+</script>
+
+<style scoped>
+  .ui.segment.head {
+    margin-bottom: 5px;
+  }
+  .ui.segment .header {
+    color: #204d74;
+  }
+  .ui.segment.body {
+    margin-top: 0;
+  }
+</style>
